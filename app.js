fe46dd3d301219622933d0029dee4ea47201e50a
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2024-01-06 12:08:50 +0500

    Math Method

---
+++ app.js
@@ -0,0 +1,142 @@
+// Question # 01
+
+// let num = +prompt(`Enter A Positive Integer`);
+
+// let roundOff = Math.round(num);
+// let floor = Math.floor(num);
+// let ceil = Math.ceil(num);
+
+// document.write(`number: ${num} <br>`);
+// document.write(`round off value: ${roundOff} <br>`);
+// document.write(`floor value: ${floor} <br>`);
+// document.write(`ceil value: ${ceil} <br>`);
+
+// Question # 02
+
+// let num = +prompt(`Enter a Negative Floating point`);
+
+// let roundOff = Math.round(num);
+// let floor = Math.floor(num);
+// let ceil = Math.ceil(num);
+
+// document.write(`number: ${num} <br>`);
+// document.write(`round off value: ${roundOff} <br>`);
+// document.write(`floor value: ${floor} <br>`);
+// document.write(`ceil value: ${ceil} <br>`);
+
+// Question # 03
+
+// let num = +prompt(`Enter a Value`)
+
+// if(num < 0){
+//     document.write(`The absolute value of ${num} is ${num * -1}`)
+// }
+
+// else{
+//     document.write(`The absolute value of ${num} is ${num}`)
+// }
+
+// Question # 04
+
+// let dice = Math.ceil(Math.random() * 6)
+
+// document.write(`random dice value: ${dice}`)
+
+// Question # 05
+
+// let dice = Math.ceil(Math.random() * 2)
+
+// if(dice == 1){
+//     document.write(`${dice} <br>`)
+//     document.write(`random coin value: Tails`);
+// }
+
+// else{
+//     document.write(`${dice} <br>`)
+//     document.write(`random coin value: Heads`);
+// }
+
+// Question # 06
+
+// let random = Math.ceil(Math.random() * 100);
+
+// document.write(`random number between 1 and 100: ${random}`)
+
+// Question # 08
+
+// let secreNumber = 7;
+// let num = +prompt(`Enter a number between 1 and 10`);
+
+// if(num === secreNumber){
+//     alert(`Congratulate!`)
+// }
+
+// else{
+//     alert(`Try Again!`)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
